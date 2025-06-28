34e3758e761c4373b0558fcbf6950bba9c692431
mhdeleglise <m.h.deleglise@gmail.com>
2025-06-28 15:30:25 +0200

    Newfiles

---
+++ Geo.py
@@ -0,0 +1,75 @@
+from math import cos as cos, sin as sin, sqrt as sqrt, pi as pi
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Vecteur:
+    def __init__(self,x=0,y=0):
+        self.x = x
+        self.y = y  
+
+    def __mul__(u,v):
+        ''' produit scalaire'''
+        return u.x * v.x + u.y * v.y
+
+    def det(u,v):
+        ''' Le determinant ''' 
+        return v.x * u.y - u.x * v.y
+
+class Point: # Espace affine euclidien de dimension 2
+    
+    def __init__(self, x=0, y=0, nom=None):
+        self.nom = nom
+        self.x = x
+        self.y = y  
+
+    def __repr__(self):
+        if self.nom is None:
+            return f"({self.x},{self.y})"
+        else:
+            return f"{self.nom}({self.x},{self.y})"
+            
+
+    def draw(self, ax, dx=0.0, dy=0.0, s=20, marker = 'o', color= 'blue', textcolor = 'blue'):
+        plt.scatter(self.x, self.y,  s=s)
+        if not self.nom is None:
+            plt.text(self.x + dx, self.y, self.nom)
+
+    def coords(self):
+        return (self.x, self.y)
+        
+    def __add__(self,other):
+        return Point(self.x + other.x, self.y + other.y)
+
+    def vecteur(self,other):
+        return Vecteur(self.x-other.x, self.y - other.y)
+
+    def distance(self,other):
+        v = Point.vecteur(self,other)
+        return sqrt(v*v)
+
+    def times(self, a):
+        ''' a est un réel, renvoie a * self '''
+        return Point(a * self.x, a * self.y)
+
+    def rotate(self, theta):
+        ''' Rotation en place le point est déplacé '''
+        x, y = self.x, self.y
+        self.x = x * cos(theta) - y * sin(theta)
+        self.y = x * sin(theta) + y * cos(theta)
+    
+    #def rotation(u,theta,x=0,y=0):
+    #    ''' Renvoie l'image de self par la rotation d'angle theta autour de (0,0) '''
+    #    return Point(x + (u.x-x) * cos(theta) - (u.y-y) * sin(theta), y + (u.x-x) * sin(theta) + (u.y-y) * cos(theta))   
+
+    def translation(self, dx, dy):
+        self.x += dx
+        self.y += dy
+
+O = Point(0,0)
+
+def rotation(u,theta,A=O):
+    ''' Renvoie l'image de u par la rotation d'angle theta autour de (0,0) '''
+    return Point(A.x + (u.x-A.x) * cos(theta) - (u.y-A.y) * sin(theta), A.y + (u.x-A.x) * sin(theta) + (u.y-A.y) * cos(theta))   
+
+    
+     
---
+++ formes2d.py
@@ -0,0 +1,32 @@
+from abc import ABC, abstractmethod
+import math
+
+# Classe abstraite : Forme2D
+class Forme2D(ABC):
+    @abstractmethod
+    def aire(self):
+        """Calcule l'aire de la forme"""
+        pass
+
+    #@abstractmethod
+    #def perimetre(self):
+    #    """Calcule le périmètre de la forme"""
+    #    pass
+
+    def afficher(self):
+        """Affiche une description générique"""
+        print(f"{self.__class__.__name__} : aire = {self.aire()}, périmètre = {self.perimetre()}")
+
+    @abstractmethod
+    def draw(self, ax):
+        """Dessine la forme sur un objet matplotlib Axes"""
+        pass
+
+    @abstractmethod
+    def translation(self, dx, dy):
+        pass
+
+    @abstractmethod
+    def rotation(self, angle_degres, centre=None):
+        """Angle en degrés, rotation autour du point 'centre' (objet Point)."""
+        pass
