921d5af6c3d273df09457ab365d6d95a2dccc6fd
mhdeleglise <m.h.deleglise@gmail.com>
2025-07-11 16:22:38 +0200

    en polaires

--- Cercles.py
+++ Cercles.py
@@ -3,9 +3,16 @@ from Geo2d import *
 
 
 class Cercle():
-    def __init__(self, centre, rayon):
+    def __init__(self, centre, r):
         self.centre = centre
-        self.rayon = rayon
+        self.rayon = r
+
+    def draw(self, ax, color=None):
+        theta = np.linspace(0, 2*np.pi, 100)
+        x1 = self.centre.x + self.rayon*np.cos(theta)
+        x2 = self.centre.y + self.rayon*np.sin(theta)
+        ax.plot(x1, x2, color=color)
+        ax.set_aspect(1)
 
     def aire(self):
         return math.pi * self.rayon ** 2
@@ -13,17 +20,9 @@ class Cercle():
     def perimetre(self):
         return 2 * math.pi * self.rayon
 
-    def draw(self, ax, fill=False, color=None, **kwds):
-        cercle = plt.Circle((self.centre.x, self.centre.y), self.rayon, fill=fill, color=color)
-        ax.add_patch(cercle)
-
     def translate(self, v):
         self.centre.translate(v)
 
     def rotation(self, angle_degres, centre=None):
         self.centre.rotation(angle_degres, centre)
-
-
-#def cercle(A,B,C):
-#    return Triangle(A,B,C).cercleCirconscrit()
         
